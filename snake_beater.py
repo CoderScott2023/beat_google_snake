c30ba5d2f5e5cfcede8407242f4cc616abd1959c
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2026-02-28 22:30:23 -0700

    Create snake_beater.py

---
+++ snake_beater.py
@@ -0,0 +1,20 @@
+import time
+import pyautogui
+
+STEP = 0.089
+
+START_DELAY = 1.0
+
+pyautogui.PAUSE = 0
+pyautogui.FAILSAFE = False
+
+def entry():
+    return
+
+def cycle():
+    return
+
+def beat_snake():
+    pyautogui.click(1282, 692)
+    entry()
+    cycle()
