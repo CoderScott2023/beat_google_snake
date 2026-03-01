3fb870164665ce229da86fcec40dc69f6aba3e06
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2026-02-28 22:55:56 -0700

    Update snake_beater.py
    Completely beats snake on small mode

--- snake_beater.py
+++ snake_beater.py
@@ -1,20 +1,54 @@
 import time
 import pyautogui
 
-STEP = 0.089
+STEP = 0.135
 
 START_DELAY = 1.0
 
 pyautogui.PAUSE = 0
-pyautogui.FAILSAFE = False
+#pyautogui.FAILSAFE = False
 
 def entry():
-    return
+    pyautogui.press("right")
+    time.sleep(STEP*6)
+    pyautogui.press("down")
+    time.sleep(STEP*4)
+
+def rightDown():
+    time.sleep(STEP*7)
+    pyautogui.press("right")
+    time.sleep(STEP)
+    pyautogui.press("down")
+def rightUp():
+    time.sleep(STEP*7)
+    pyautogui.press("right")
+    time.sleep(STEP)
+    pyautogui.press("up")
 
 def cycle():
-    return
+    pyautogui.press("left")
+    time.sleep(STEP*9)
+    pyautogui.press("up")
+
+    time.sleep(STEP*8)
+    pyautogui.press("right")
+    time.sleep(STEP)
+    pyautogui.press("down")
+
+    rightUp()
+    rightDown()
+    rightUp()
+    rightDown()
+    rightUp()
+    rightDown()
+    rightUp()
+    rightDown()
+    time.sleep(STEP*8)
 
 def beat_snake():
     pyautogui.click(1282, 692)
     entry()
-    cycle()
+    while True:
+        cycle()
+
+beat_snake()
